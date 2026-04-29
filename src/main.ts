159fe3c335666e2e20b771ecc202c197d5cf3cb3
Sy-datastudio <syedafathima006@gmail.com>
2026-04-29 18:13:40 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+npm install @fontsource/inter @fontsource/inter-tight
 
 createApp(App).mount('#app')
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+
+import '@fontsource/inter-tight/600.css'
+import '@fontsource/inter-tight/700.css'
+import '@fontsource/inter-tight/800.css'
